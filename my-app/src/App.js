d5ddebc9a17a6d53be0e00e2dbecfc4f35d35046
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-01-29 01:55:35 +0100

    project config and structure

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './scss/main.scss';
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+// import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
