a2ba848a628e9b60353af3ba17168dc3bc837464
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-01-31 00:32:30 +0100

    basic routing-Home

---
+++ my-app/src/components/HomeHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomeHeader() {
+    return (
+        <nav className={"homeHeaderNav"}>
+            <span>Start</span>
+            <span>O co chodzi?</span>
+            <span>O nas</span>
+            <span>Fundacja i Oranizacje</span>
+            <span>Kontakt</span>
+        </nav>
+    )
+}
+
+export default HomeHeader
