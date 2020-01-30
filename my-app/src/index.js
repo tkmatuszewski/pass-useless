31ac3c3c9ae02ed9a4b0213e14e2af45a060def6
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-01-31 00:27:24 +0100

    project colors added to colors partial

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./scss/main.scss";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
-import url('https://fonts.googleapis.com/css?family=Merriweather|Open+Sans&display=swap');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
