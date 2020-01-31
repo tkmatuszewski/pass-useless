91acfcf48fcbbb4400a836cf31b3eeb868292fb6
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-01-31 02:01:28 +0100

    HomeHeader component structure and grid alignment

--- my-app/src/components/HomeHeader.js
+++ my-app/src/components/HomeHeader.js
@@ -2,13 +2,29 @@ import React from "react";
 
 function HomeHeader() {
     return (
-        <nav className={"homeHeaderNav"}>
-            <span>Start</span>
-            <span>O co chodzi?</span>
-            <span>O nas</span>
-            <span>Fundacja i Oranizacje</span>
-            <span>Kontakt</span>
-        </nav>
+        <div className={"homeHeader_main"}>
+            <div className={"homeHeader_menu"}>
+                <div className={"homeHeader_logIn"}>
+                    <button className={"homeHeader_logBtn"}>Zaloguj</button>
+                    <button className={"homeHeader_logBtn"}>Załóż konto</button>
+                </div>
+                <nav className={"homeHeader_nav"}>
+                    <span>Start</span>
+                    <span>O co chodzi?</span>
+                    <span>O nas</span>
+                    <span>Fundacja i Oranizacje</span>
+                    <span>Kontakt</span>
+                </nav>
+            </div>
+            <div className={"homeHeader_banner"}>
+                <h2>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h2>
+                <div>
+                    <button>Oddaj rzeczy</button>
+                    <button>Zorganizuj zbiórkę</button>
+                </div>
+            </div>
+            <div className={"homeHeader_img"}></div>
+        </div>
     )
 }
 
