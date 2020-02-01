dcbc6f9e0e96ee29bdfd63bac3068256ee18ff4e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-01 02:17:04 +0100

    HomeHeader component done -no routing in lower menu

--- my-app/src/components/HomeHeader.js
+++ my-app/src/components/HomeHeader.js
@@ -1,15 +1,26 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {Link as Blink} from "react-scroll"
 
 function HomeHeader() {
+    // state = {
+    //   active : false
+    // };
+    // function activate  {
+    //     this.setState({active: !this.state.active});
+    //     if (this.state.active) {
+    //         this.className={"active"}
+    //     }
+    // };
     return (
         <div className={"homeHeader_main"}>
             <div className={"homeHeader_menu"}>
                 <div className={"homeHeader_logIn"}>
-                    <button className={"homeHeader_logBtn"}>Zaloguj</button>
-                    <button className={"homeHeader_logBtn"}>Załóż konto</button>
+                    <div className={"homeHeader_logBtn"}><Link to="/logowanie">Zaloguj</Link></div>
+                    <div className={"homeHeader_rgstr"}><Link to="/rejestracja">Załóż konto</Link></div>
                 </div>
                 <nav className={"homeHeader_nav"}>
-                    <span>Start</span>
+                    <span><Blink to={"/start"}> Start </Blink></span>
                     <span>O co chodzi?</span>
                     <span>O nas</span>
                     <span>Fundacja i Oranizacje</span>
@@ -17,10 +28,14 @@ function HomeHeader() {
                 </nav>
             </div>
             <div className={"homeHeader_banner"}>
-                <h2>Zacznij pomagać! Oddaj niechciane rzeczy w zaufane ręce</h2>
-                <div>
-                    <button>Oddaj rzeczy</button>
-                    <button>Zorganizuj zbiórkę</button>
+                <div className={"homeHeader_bannerCnt"}>
+                    <h2 className={"homeHeader_bannerTitle"}><span>Zacznij pomagać!</span>
+                        <span>Oddaj niechciane rzeczy w zaufane ręce</span></h2>
+                    <div className={"homeHeader_bannerDecoration"}></div>
+                </div>
+                <div className={"homeHeader_bannerButtons"}>
+                    <div className={"homeHeader_bannerBtnL"}><Link to="/logowanie"><span>Oddaj</span><span>rzeczy</span></Link></div>
+                    <div className={"homeHeader_bannerBtnR"}><Link to="/logowanie"><span>Zorganizuj</span><span>zbiórkę</span></Link></div>
                 </div>
             </div>
             <div className={"homeHeader_img"}></div>
