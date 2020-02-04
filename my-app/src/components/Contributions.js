7142b657c32c6dfceabe8de734ab72c2cf72802f
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-04 19:42:50 +0100

    firebase functionality included

--- my-app/src/components/Contributions.js
+++ my-app/src/components/Contributions.js
@@ -1,73 +1,172 @@
-import React from "react";
+import React, {Component} from "react";
 import {Link as Slink, Element, Events, animateScroll as scroll, scrollSpy, scroller} from 'react-scroll';
+import data from "../firebase";
 
-function Contributions() {
-    return (
-        <div className="contributions_main">
-            <div className="contributions_cnt">
-                <div className="contributions_top">
-                    <div className="contributions_title">Komu pomagamy?
-                        <div className="contributions_decor"></div>
-                    </div>
-                    <div className="contributions_list">
-                        <div className="contributions_list_item">Fundacjom</div>
-                        <div className="contributions_list_item">Organizacjom pozarządowym</div>
-                        <div className="contributions_list_item">Lokalnym zbiórkom</div>
-                    </div>
-                    <div className="contributions_info">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z
-                        którymi
-                        współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
+class Contributions extends Component {
+
+    state = {
+        charities: [],
+        ngos: [],
+        localFunds: [],
+        active: {name: "charities"},
+        listCount: 0
+    };
+
+    handleClick = (e) => {
+        this.setState({active: e.target.dataset});
+        if (this.state.active.name === "charities") {
+            this.setState({listCount: this.state.charities.length})
+        }
+        if (this.state.active.name === "ngos") {
+            this.setState({listCount: this.state.ngos.length})
+        }
+        if (this.state.active.name === "localFunds") {
+            this.setState({listCount: this.state.localFunds.length})
+        }
+    };
+    bottomContent = () => {
+        if (this.state.active.name === "charities") {
+            console.log("charity");
+            return this.state.charities.map((el) => {
+                return <Charity e={el}/>
+            })
+        } else if (this.state.active.name === "ngos") {
+            console.log("ngo");
+            return this.state.ngos.map((el) => {
+                return <Ngo e={el}/>
+            })
+        } else if (this.state.active.name === "localFunds") {
+            console.log("local fund");
+            return this.state.localFunds.map((el) => {
+                return <LocalFund e={el}/>
+            })
+        }
+    };
+
+    render() {
+        return (
+            <div className="contributions_main" id="contributions">
+                <div className="contributions_cnt">
+                    <div className="contributions_top">
+                        <div className="contributions_title">Komu pomagamy?
+                            <div className="contributions_decor"></div>
+                        </div>
+                        <div className="contributions_list">
+                            <div className="contributions_list_item" onClick={this.handleClick}
+                                 data-name="charities">Fundacjom
+                            </div>
+                            <div className="contributions_list_item" onClick={this.handleClick}
+                                 data-name={"ngos"}>Organizacjom pozarządowym
+                            </div>
+                            <div className="contributions_list_item" onClick={this.handleClick}
+                                 data-name="localFunds">Lokalnym zbiórkom
+                            </div>
+                        </div>
+                        <div className="contributions_info">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z
+                            którymi
+                            współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
+                        </div>
                     </div>
                 </div>
+                <div className="contributions_bottom">
+                    {this.bottomContent()}
+                </div>
+                {/*<div className="contributions_pagination">*/}
+                {/*    <button className="contributions_pagination_btn">1</button>*/}
+                {/*    <button className="contributions_pagination_btn">2</button>*/}
+                {/*    <button className="contributions_pagination_btn">3</button>*/}
+                {/*</div>*/}
             </div>
-            <div className="contributions_bottom">
-                <div className="contributions_item">
-                    <div className="contributions_item_left">
-                        <div className="contributions_item_left_title">
-                            Fundacja “Dbam o Zdrowie”
-                        </div>
-                        <div className="contributions_item_left_target">
-                            Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.
-                        </div>
-                    </div>
-                    <div className="contributions_item_left_right">
-                        ubrania, jedzenie, sprzęt AGD, meble, zabawki
-                    </div>
-                </div>
-                <div className="contributions_item">
-                    <div className="contributions_item_left">
-                        <div className="contributions_item_left_title">
-                            Fundacja “Dbam o Zdrowie”
-                        </div>
-                        <div className="contributions_item_left_target">
-                            Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.
-                        </div>
-                    </div>
-                    <div className="contributions_item_left_right">
-                        ubrania, jedzenie, sprzęt AGD, meble, zabawki
-                    </div>
-                </div>
-                <div className="contributions_item">
-                    <div className="contributions_item_left">
-                        <div className="contributions_item_left_title">
-                            Fundacja “Dbam o Zdrowie”
-                        </div>
-                        <div className="contributions_item_left_target">
-                            Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.
-                        </div>
-                    </div>
-                    <div className="contributions_item_left_right">
-                        ubrania, jedzenie, sprzęt AGD, meble, zabawki
-                    </div>
-                </div>
-            </div>
-            <div className="contributions_pagination">
-                <button className="contributions_pagination_btn">1</button>
-                <button className="contributions_pagination_btn">2</button>
-                <button className="contributions_pagination_btn">3</button>
-            </div>
-        </div>
-    )
+        )
+    }
+
+    componentDidMount() {
+        data.collection(`charities`).get().then(
+            (el) => {
+                el.docs.map((doc) => {
+                    this.setState({
+                        charities: this.state.charities.concat(doc.data()),
+                    });
+                })
+            });
+        data.collection(`NGOs`).get().then(
+            (el) => {
+                el.docs.map((doc) => {
+                    this.setState({
+                        ngos: this.state.ngos.concat(doc.data()),
+                    });
+                })
+            });
+        data.collection(`localFunds`).get().then(
+            (el) => {
+                el.docs.map((doc) => {
+                    this.setState({
+                        localFunds: this.state.localFunds.concat(doc.data()),
+                    });
+                })
+            });
+    }
 }
+
+class Charity extends Component {
+    render() {
+        return (
+            <div className="contributions_item">
+                <div className="contributions_item_left">
+                    <div className="contributions_item_left_title">
+                        {this.props.e.name}
+                    </div>
+                    <div className="contributions_item_left_target">
+                        Cel i misja: {this.props.e.mission}
+                    </div>
+                </div>
+                <div className="contributions_item_left_right">
+                    {this.props.e.items.join(", ")}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Ngo extends Component {
+    render() {
+        return (
+            <div className="contributions_item">
+                <div className="contributions_item_left">
+                    <div className="contributions_item_left_title">
+                        {this.props.e.name}
+                    </div>
+                    <div className="contributions_item_left_target">
+                        Cel i misja: {this.props.e.mission}
+                    </div>
+                </div>
+                <div className="contributions_item_left_right">
+                    {this.props.e.items.join(", ")}
+                </div>
+            </div>
+        )
+    }
+}
+
+class LocalFund extends Component {
+    render() {
+        return (
+            <div className="contributions_item">
+                <div className="contributions_item_left">
+                    <div className="contributions_item_left_title">
+                        {this.props.e.name}
+                    </div>
+                    <div className="contributions_item_left_target">
+                        Cel i misja: {this.props.e.mission}
+                    </div>
+                </div>
+                <div className="contributions_item_left_right">
+                    {this.props.e.items.join(", ")}
+                </div>
+            </div>
+        )
+    }
+}
+
 
 export default Contributions
